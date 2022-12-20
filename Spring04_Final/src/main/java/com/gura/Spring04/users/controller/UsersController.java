834b38d0976b7eb50ddbf96bb1a3a380cbad69de
CheonOkHui <cjsdhrgml000@gmail.com>
2022-12-20 15:06:16 +0900

    Spring04_Fianl 추가하고, 회원가입, 로그인 구현함.

---
+++ Spring04_Final/src/main/java/com/gura/Spring04/users/controller/UsersController.java
@@ -0,0 +1,70 @@
+package com.gura.Spring04.users.controller;
+
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.Spring04.users.dto.UsersDto;
+import com.gura.Spring04.users.service.UsersService;
+
+@Controller
+public class UsersController {
+	
+	@Autowired
+	private UsersService service;
+	
+	/*
+	 * 	GET 방식 /users/signup_form 요청을 처리할 메소드
+	 * 	- 요청 방식이 다르면 실행되지 않는다. 
+	 */
+	@RequestMapping(method = RequestMethod.GET, value="/users/signup_form")
+	public String signupForm() {
+		
+		return "users/signup_form";
+	}
+	
+	// 회원 가입 요청처리
+	@RequestMapping(method = RequestMethod.POST, value="/users/signup")
+	public ModelAndView signup(ModelAndView mView, UsersDto dto) {
+		service.addUser(dto);
+		mView.setViewName("users/signup");
+		return mView;
+	}
+	
+	// 로그인 폼 요청 처리
+	@RequestMapping(method = RequestMethod.GET, value = "/users/loginform")
+	public String loginform() {
+		return "users/loginform";
+	}
+	
+	@RequestMapping("/users/login")
+	public ModelAndView login(ModelAndView mView, UsersDto dto, String url, HttpSession session) {
+		/*
+		 * 	서비스에서 비즈니스 로직을 처리할 때 필요로 하는 객체를 컨트롤러에서 직접 전달을 해줘야한다.
+		 * 	주로, HttpServletRequest, HttpServletResponse, HttpSession, ModelAndView
+		 * 	등등의 챛게이다.
+		 */
+		service.loginProcess(dto, session);
+		
+		// 로그인 후에 가야할 목적지 정보를 인코딩 하지 않는 것과 인코딩 한 것을 모두 ModelAndView 객체에 담고
+		String encodedUrl = URLEncoder.encode(url);
+		mView.addObject("url",url);
+		mView.addObject("encodedUrl", encodedUrl);
+		
+		// view page로 forward 이동해서 응답한다.
+		mView.setViewName("users/login");
+		return mView;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/users/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "users/logout";
+	}
+}
