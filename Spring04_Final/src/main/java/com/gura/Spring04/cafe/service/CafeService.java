30708d281c9d3bcfca8de4bfddeb7d33ada0b5de
CheonOkHui <cjsdhrgml000@gmail.com>
2022-12-26 17:24:31 +0900

    Spring04에  댓글관련기능 일부 추가

--- Spring04_Final/src/main/java/com/gura/Spring04/cafe/service/CafeService.java
+++ Spring04_Final/src/main/java/com/gura/Spring04/cafe/service/CafeService.java
@@ -3,6 +3,7 @@ package com.gura.Spring04.cafe.service;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.gura.Spring04.cafe.dto.CafeCommentDto;
 import com.gura.Spring04.cafe.dto.CafeDto;
 
 
@@ -12,5 +13,10 @@ public interface CafeService {
 	public void saveContent(CafeDto dto);
 	public void updateContent(CafeDto dto);
 	public void deleteContent(int num, HttpServletRequest request);
-	public void getData(HttpServletRequest request); //글 수정하기 위해 정보 불러오는 기능
+	public void getData(HttpServletRequest request); // 글 수정하기 위해 정보 불러오는 기능
+	
+	public void saveComment(HttpServletRequest request); // 댓글 저장
+	public void deleteComment(HttpServletRequest request); // 댓글 삭제
+	public void updateComment(CafeCommentDto dto); // 댓글 수정
+	public void moreCommentList(HttpServletRequest request); // 댓글 더보기 기능
 }
