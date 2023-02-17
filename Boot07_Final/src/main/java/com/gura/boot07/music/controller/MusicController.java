43e2eb29b4c15bdf61d7ca2a0fc084e689c142ca
CheonOkHui <cjsdhrgml000@gmail.com>
2023-02-17 09:37:08 +0900

    Android app 연동 기능 구현중...

---
+++ Boot07_Final/src/main/java/com/gura/boot07/music/controller/MusicController.java
@@ -0,0 +1,31 @@
+package com.gura.boot07.music.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.boot07.music.service.MusicService;
+
+@Controller
+public class MusicController {
+	@Autowired MusicService service;
+	// 음악파일 업로드 폼 요청처리
+	@RequestMapping("/music/insertForm")
+	public String insertForm() {
+		
+		return "music/insertForm";
+	}
+	// 음악파일 업로드 요청처리
+	@RequestMapping("/music/insert")
+	public String insert(MultipartFile file, HttpServletRequest request) {
+		
+		service.saveFile(file, request);
+		
+		return "redirect:/music/list";
+	}
+	
+}
--- Boot07_Final/src/main/webapp/WEB-INF/views/table.sql
+++ Boot07_Final/src/main/webapp/WEB-INF/views/table.sql
@@ -86,3 +86,16 @@ CREATE TABLE board_gallery(
 );
 
 CREATE SEQUENCE board_gallery_seq;
+
+-- mp3 목록을 관리하기 위한 테이블
+CREATE TABLE board_music(
+    num NUMBER PRIMARY KEY,
+    writer VARCHAR2(100), -- 서버에 로그인된 아이디
+    title VARCHAR2(100), --  mp3 파일의 meta data에서 추출한 title
+    artist VARCHAR2(100),--  mp3 파일의 meta data에서 추출한 musician
+    orgFileName VARCHAR2(100),
+    saveFileName VARCHAR2(100),
+    regdate DATE    
+);
+
+CREATE SEQUENCE board_music_seq;
